75d9a352fbb524eeb39a74980b37efdfdb550eb0
Julia Viehbaeck <julia.viehbaeck@gmail.com>
2020-11-18 16:12:28 +0100

    did a very ugly looking restaurant rails

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# A visitor can see the list of all restaurants.
+  resources :restaurants, only: [:new, :create, :show, :index] do
+    resources :reviews, only: [:new, :create]
+  end
+# A visitor can add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
 end
